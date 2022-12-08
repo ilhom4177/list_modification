e19b1557ed64e02f0a3a09c2bf0a3aebdf8a928d
Ilhom <ilhommamatqulov276@gmail.com>
2022-12-08 20:18:24 +0500

    list_mod04.py

--- list_mod04.py
+++ list_mod04.py
@@ -7,4 +7,10 @@ def remove_positive(lst):
     Returns:
         list: list without positive numbers.
     '''
-    return
+    l = []
+    for i in lst:
+        if i<0:
+            l.append(i)
+
+    return l
+print(remove_positive([1, -2, 3, -4, 5]))
