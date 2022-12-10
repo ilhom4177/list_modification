2f9a26aa314192f9e9719f5f8ce985c95e7fadb5
Ilhom <ilhommamatqulov276@gmail.com>
2022-12-10 12:23:48 +0500

    list_mod09.py

--- list_mod09.py
+++ list_mod09.py
@@ -7,4 +7,10 @@ def square_and_remove_positive(lst):
     Returns:
         list: list of all non-negative numbers ared squared.
     '''
-    return
+    list1=[]
+    for i in range(len(lst)):
+        if lst[i]<0:
+            x=lst[i]**2
+            list1.append(x)
+    return list1
+print(square_and_remove_positive([1, -2, 3, -4, 5]))
